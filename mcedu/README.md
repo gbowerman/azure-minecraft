ae76103493dc55974f0ee2f7c1d5532ceb296fb1
Guy Bowerman <guybo@outlook.com>
2017-05-26 11:44:46 -0700

    Basic Minecraft PE settings

--- mcedu/README.md
+++ mcedu/README.md
@@ -5,5 +5,5 @@ This template deploys and sets up a customized Minecraft PE server on an Ubuntu 
 
 You can set common Minecraft server properties as parameters at deployment time. Once the deployment is successful you can connect to the DNS address of the VM with a Minecraft Windows 10 Edition launcher. 
 
-The following Minecraft server configuration parameters can be set at deployment time: Minecraft user name, difficulty, level-name, gamemode, white-list, enable-command-block, spawn-monsters, generate-structures, level-seed.
+The following Minecraft PE server configuration parameters can be set at deployment time: XBox Live XUID, difficulty, level-name, gamemode, white-list,  level-seed.
 
--- mcedu/install_minecraft.sh
+++ mcedu/install_minecraft.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-# Custom Minecraft server install script for Ubuntu 16.04-LTS
-# $1 = Minecraft user name
+# Custom Minecraft PE server install script for Ubuntu 16.04-LTS
+# $1 = XUID
 # $2 = difficulty
 # $3 = level-name
 # $4 = gamemode
 # $5 = white-list
-# $6 = enable-command-block
-# $7 = spawn-monsters
-# $8 = generate-structures
-# $9 = level-seed
+# $6 = level-seed
 
 # basic service and API settings
-minecraft_server_path=/srv/minecraft_server
+minecraft_root_path=/srv/minecraft_server
+minecraft_server_path=$minecraft_root_path/dedicated_server/output
 minecraft_user=minecraft
 minecraft_group=minecraft
-UUID_URL=https://api.mojang.com/users/profiles/minecraft/$1
 
 # install required software (unzip and libcurl4)
 while ! echo y | apt-get update; do
@@ -35,60 +32,50 @@ done
 # create user and install folder
 adduser --system --no-create-home --home /srv/minecraft-server $minecraft_user
 addgroup --system $minecraft_group
-mkdir $minecraft_server_path
-cd $minecraft_server_path
+mkdir $minecraft_root_path
+cd $minecraft_root_path
 
 # set permissions on install folder
-chown -R $minecraft_user $minecraft_server_path
+chown -R $minecraft_user $minecraft_root_path
 
 # get and unzip the server zip file
-curl "https://github.com/gbowerman/azure-minecraft/blob/master/mcedu/server/dedicated_server.zip?raw=true" > $minecraft_server_path/dedicated_server.zip
-unzip $minecraft_server_path/dedicated_server.zip -d $minecraft_server_path
-chmod +x $minecraft_server_path/dedicated_server/output/mcpe_server
+curl "https://github.com/gbowerman/azure-minecraft/blob/master/mcedu/server/dedicated_server.zip?raw=true" > $minecraft_root_path/dedicated_server.zip
+unzip $minecraft_root_path/dedicated_server.zip -d $minecraft_root_path
+chmod +x $minecraft_server_path/mcpe_server
 
 # create a launch file
-touch $minecraft_server_path/dedicated_server/output/launch.sh
-printf 'cd %s/dedicated_server/output\n' $minecraft_server_path >> $minecraft_server_path/dedicated_server/output/launch.sh
-printf 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:%s/dedicated_server/output\n' $minecraft_server_path >> $minecraft_server_path/dedicated_server/output/launch.sh
-printf './mcpe_server' >> $minecraft_server_path/dedicated_server/output/launch.sh
-chmod +x $minecraft_server_path/dedicated_server/output/launch.sh
+touch $minecraft_server_path/launch.sh
+printf 'cd %s\n' $minecraft_server_path >> $minecraft_server_path/launch.sh
+printf 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:%s\n' $minecraft_server_path >> $minecraft_server_path/launch.sh
+printf './mcpe_server' >> $minecraft_server_path/launch.sh
+chmod +x $minecraft_server_path/launch.sh
 
 # create a service
 touch /etc/systemd/system/minecraft-server.service
 printf '[Unit]\nDescription=Minecraft Service\nAfter=rc-local.service\n' >> /etc/systemd/system/minecraft-server.service
 printf '[Service]\nWorkingDirectory=%s\n' $minecraft_server_path >> /etc/systemd/system/minecraft-server.service
-printf 'ExecStart=%s/dedicated_server/output/launch.sh\n' $minecraft_server_path >> /etc/systemd/system/minecraft-server.service
+printf 'ExecStart=%s/launch.sh\n' $minecraft_server_path >> /etc/systemd/system/minecraft-server.service
 printf 'ExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nRestart=on-failure\n' >> /etc/systemd/system/minecraft-server.service
 printf '[Install]\nWantedBy=multi-user.target\nAlias=minecraft-server.service' >> /etc/systemd/system/minecraft-server.service
 chmod +x /etc/systemd/system/minecraft-server.service
 
 # create and set permissions on user access JSON files
-touch $minecraft_server_path/banned-players.json
-chown $minecraft_user:$minecraft_group $minecraft_server_path/banned-players.json
-touch $minecraft_server_path/banned-ips.json
-chown $minecraft_user:$minecraft_group $minecraft_server_path/banned-ips.json
 touch $minecraft_server_path/whitelist.json
+printf '[\n {\n  \"xuid\":\"%s\", \"ignoresPlayerLimit\":true\n }\n]' $1 >> $minecraft_server_path/whitelist.json
 chown $minecraft_user:$minecraft_group $minecraft_server_path/whitelist.json
 
 # create a valid operators file using the Mojang API
 touch $minecraft_server_path/ops.json
-mojang_output="`wget -qO- $UUID_URL`"
-rawUUID=${mojang_output:7:32}
-UUID=${rawUUID:0:8}-${rawUUID:8:4}-${rawUUID:12:4}-${rawUUID:16:4}-${rawUUID:20:12}
-printf '[\n {\n  \"uuid\":\"%s\",\n  \"name\":\"%s\",\n  \"level\":4\n }\n]' $UUID $1 >> $minecraft_server_path/ops.json
+printf '[\n {\n  \"xuid\":\"%s\"\n }\n]' $1 >> $minecraft_server_path/ops.json
 chown $minecraft_user:$minecraft_group $minecraft_server_path/ops.json
 
 # set user preferences in server.properties
 touch $minecraft_server_path/server.properties
 chown $minecraft_user:$minecraft_group $minecraft_server_path/server.properties
-# echo 'max-tick-time=-1' >> $minecraft_server_path/server.properties
 printf 'difficulty=%s\n' $2 >> $minecraft_server_path/server.properties
 printf 'level-name=%s\n' $3 >> $minecraft_server_path/server.properties
 printf 'gamemode=%s\n' $4 >> $minecraft_server_path/server.properties
 printf 'white-list=%s\n' $5 >> $minecraft_server_path/server.properties
-printf 'enable-command-block=%s\n' $6 >> $minecraft_server_path/server.properties
-printf 'spawn-monsters=%s\n' $7 >> $minecraft_server_path/server.properties
-printf 'generate-structures=%s\n' $8 >> $minecraft_server_path/server.properties
-printf 'level-seed=%s\n' $9 >> $minecraft_server_path/server.properties
+printf 'level-seed=%s\n' $6 >> $minecraft_server_path/server.properties
 
 systemctl start minecraft-server
