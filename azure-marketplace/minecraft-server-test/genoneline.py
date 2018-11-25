4e4215ca2faabd0ca237a17ae150f976f1ab936a
Guy Bowerman <guybo@outlook.com>
2018-11-24 19:34:50 -0800

    Converting Anthony's script converter to Python3

---
+++ azure-marketplace/minecraft-server-test/genoneline.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+'''goal "commandToExecute": "[variables('jumpboxWindowsCustomScript')]"'''
+import os
+import re
+import sys
+
+
+def convertToOneArmTemplateLine(file):
+    with open(file) as f:
+        content = f.read()
+
+    # convert to one line
+    content = content.replace("\\", "\\\\")
+    content = content.replace("\r\n", "\\n")
+    content = content.replace("\n", "\\n")
+    content = content.replace('"', '\\"')
+
+    # replace {{{ }}} with variable names
+    return re.sub(r"{{{([^}]*)}}}", r"',variables('\1'),'", content)
+
+
+def usage():
+    print('    usage: ', os.path.basename(sys.argv[0]), 'file1')
+    print('    builds a one line string to send to commandToExecute')
+
+
+def main():
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit(1)
+
+    file = sys.argv[1]
+    if not os.path.exists(file):
+        sys.exit('Error: file: ' + file + ' does not exist')
+
+    # build the yml file for cluster
+    oneline = convertToOneArmTemplateLine(file)
+
+    print('"customData": "[base64(concat(\'' + oneline + '\'))]\",')
+
+if __name__ == "__main__":
+    main()
